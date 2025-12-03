d6c956da948731fa62d804bc419e0f2285c0c169
Christian Maulany <cmaulany@gmail.com>
2025-12-03 01:30:05 +0100

    day 1 and 2

---
+++ day1.ts
@@ -0,0 +1,62 @@
+const exampleInput = await Deno.readTextFile("examples/day1.txt");
+const input = await Deno.readTextFile("inputs/day1.txt");
+
+type Movement = {
+    direction: "L" | "R";
+    amount: number;
+}
+
+type State = {
+    count: number;
+    position: number;
+};
+
+const parseInput = (input: string): Movement[] => input.split('\n').map((line) => {
+    const direction = line[0] as "L" | "R";
+    const amount = Number(line.slice(1));
+    return { direction, amount };
+});
+
+const moveDial = (state: State, movement: Movement): State => {
+    let { count, position } = state;
+    const d = movement.direction === "L" ? -1 : 1;
+    position += d * movement.amount;
+    position %= 100;
+    if (position < 0) {
+        position += 100;
+    }
+    if (position === 0) {
+        count++;
+    }
+    return { count, position };
+}
+
+const tickDial = (state: State, movement: Movement): State => {
+    let { count, position, } = state;
+    const d = movement.direction === "L" ? -1 : 1;
+    for (let i = 0; i < movement.amount; i++) {
+        position += d;
+        position %= 100;
+        if (position < 0) {
+            position += 100;
+        }
+        if (position === 0) {
+            count++;
+        }
+    }
+    return { count, position };
+}
+
+const run = (input: string) => {
+    const initialState: State = {
+        count: 0,
+        position: 50,
+    };
+    const movements = parseInput(input);
+    const part1 = movements.reduce(moveDial, initialState).count;
+    const part2 = movements.reduce(tickDial, initialState).count;
+    return { part1, part2 };
+}
+
+console.log('Example', run(exampleInput));
+console.log('Result', run(input));
---
+++ day2.ts
@@ -0,0 +1,43 @@
+const exampleInput = await Deno.readTextFile("examples/day2.txt");
+const input = await Deno.readTextFile("inputs/day2.txt");
+
+type Range = [number, number];
+
+const parseInput = (input: string): Range[] =>
+    input.split(',')
+        .map((str) => str.split('-').map(Number) as [number, number]);
+
+const expandRange = ([start, end]: [number, number]) =>
+    Array.from({ length: end - start }, (_, i) => Number(start) + i);
+
+const isRepeatingOnce = (id: number) => {
+    const idString = id.toString();
+    const middle = Math.floor(idString.length / 2);
+    const lhs = idString.slice(0, middle);
+    const rhs = idString.slice(middle);
+    return lhs === rhs;
+}
+
+const isRepeating = (id: number) => {
+    const idString = id.toString();
+    for (let i = 1; i < idString.length; i++) {
+        const seq = idString.slice(0, i);
+        const isInvalid = idString.split(seq).join('').length === 0;
+        if (isInvalid) {
+            return true;
+        }
+    }
+    return false;
+}
+
+const sum = (ns: number[]) => ns.reduce((a, b) => a + b);
+
+const run = (input: string) => {
+    const ranges = parseInput(input);
+    const part1 = sum(ranges.flatMap(expandRange).filter(isRepeatingOnce));
+    const part2 = sum(ranges.flatMap(expandRange).filter(isRepeating));
+    return { part1, part2 };
+}
+
+console.log('Example', run(exampleInput));
+console.log('Result', run(input));
