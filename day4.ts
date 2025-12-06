a6838ff2a8ae560d9c9a40b45fd58d424e8de2be
Christian Maulany <cmaulany@gmail.com>
2025-12-06 14:19:35 +0100

    day4

---
+++ day4.ts
@@ -0,0 +1,76 @@
+const exampleInput = await Deno.readTextFile("examples/day4.txt");
+const input = await Deno.readTextFile("inputs/day4.txt");
+
+type PaperRoll = '@';
+type Empty = '.';
+
+type Tile = PaperRoll | Empty;
+type Grid = Tile[][];
+type Position = { x: number; y: number };
+
+const parseInput = (input: string): Grid => input.split('\n').map((line) => line.split('') as Tile[]);
+
+const NEIGHBOR_DELTAS = [
+    { x: -1, y: -1 },
+    { x: 0, y: -1 },
+    { x: 1, y: -1 },
+    { x: -1, y: 0 },
+    { x: 1, y: 0 },
+    { x: -1, y: 1 },
+    { x: 0, y: 1 },
+    { x: 1, y: 1 },
+];
+
+const getNeighbors = (grid: Grid, position: Position) => {
+    const width = grid[0].length;
+    const height = grid.length;
+    const { x, y } = position;
+
+    return NEIGHBOR_DELTAS
+        .map((delta) => ({
+            x: x + delta.x,
+            y: y + delta.y,
+        }))
+        .filter(({ x, y }) => (
+            x >= 0
+            && y >= 0
+            && x < width
+            && y < height
+        ));
+};
+
+const countPaperRolls = (grid: Grid) => grid.flat().filter((tile) => tile === '@').length;
+
+const removeAccessibleRolls = (grid: Grid) => {
+    return grid.map((line, y) => line.map((tile, x) => {
+        if (tile === '.') {
+            return tile;
+        }
+        const paperRollNeighbors = getNeighbors(grid, { x, y })
+            .filter(({ x, y }) => grid[y][x] === '@');
+        if (paperRollNeighbors.length < 4) {
+            return '.';
+        }
+        return tile;
+    }));
+};
+
+const removeAllAccessibleRolls = (grid: Grid) => {
+    let previousCount = Infinity;
+    while (countPaperRolls(grid) < previousCount) {
+        previousCount = countPaperRolls(grid);
+        grid = removeAccessibleRolls(grid);
+    }
+    return grid;
+}
+
+const run = (input: string) => {
+    const grid = parseInput(input);
+    const initialCount = countPaperRolls(grid);
+    const part1 = initialCount - countPaperRolls(removeAccessibleRolls(grid));
+    const part2 = initialCount - countPaperRolls(removeAllAccessibleRolls(grid));
+    return { part1, part2 };
+}
+
+console.log('Example', run(exampleInput));
+console.log('Result', run(input));
